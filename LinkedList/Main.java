4197b0c3c3f6a1408797daebdecc90a47d3a81ba
krishna <dhekkrishna1@gmail.com>
2023-07-29 15:47:30 +0530

    Singly Linked List with a node is created

---
+++ LinkedList/Main.java
@@ -0,0 +1,10 @@
+package LinkedList;
+
+public class Main {
+    public static void main(String[] args) {
+        SinglyLinkedList sll = new SinglyLinkedList();
+        sll.createSinglyLinkedList(1);
+        System.out.println(sll.head.value);
+    }
+    
+}
---
+++ LinkedList/Node.java
@@ -0,0 +1,5 @@
+package LinkedList;
+public class  Node{
+    public int value;
+    public Node next;
+}
