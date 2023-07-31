db1cbfcd1e07021c4622fe3c03e213c1efbd5314
krishna <dhekkrishna1@gmail.com>
2023-08-01 01:11:25 +0530

    Created pop method of stack

--- javaCoding/Array/Main.java
+++ javaCoding/Array/Main.java
@@ -4,6 +4,15 @@ public class Main {
    public static void main(String[] args) {
     Stack s = new Stack(5);
     System.out.println("Is stack empty ? "+ s.isEmpty());
+    System.out.println("Is stack full ? "+ s.isFull());
+    s.Push(1);
+    s.Push(2);
+    s.Push(27);
+    s.Push(78);
+    s.Push(3);
+    s.Push(9);//stack is full
+    System.out.println("Popped element is :"+s.Pop());
+  
    }
     
 }
--- javaCoding/Array/Stack.java
+++ javaCoding/Array/Stack.java
@@ -19,4 +19,40 @@ public class Stack {
         return false;
     }
     
+    //isFull
+    public boolean isFull(){
+        if(topOfStack==arrStack.length-1){
+            return true;
+        }
+        return false;
+    }
+
+    //push method
+    public void Push(int value){
+        if(isFull()){
+            System.out.println("Stack is full");
+        }
+        else{
+            topOfStack++;
+            arrStack[topOfStack] = value;
+            System.out.println("Value is successfully added "+value);
+
+        }
+    }
+    //pop method
+    public int Pop(){
+        if(isEmpty()){
+            System.out.println("Stack is empty!");
+            return -1;
+        }
+        else{
+            int topValue = arrStack[topOfStack];
+            topOfStack--;
+            return topValue;
+        }
+    }
+    //peek method
+
+    
+    
 }
