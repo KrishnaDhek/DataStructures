99787308cec5ebfada409d2a2f86104d1a451fb3
krishna <dhekkrishna1@gmail.com>
2023-08-01 18:26:44 +0530

    Added Preorder Traversal method

--- javaCoding/Tree/BinaryTreeUsingLinkedList.java
+++ javaCoding/Tree/BinaryTreeUsingLinkedList.java
@@ -8,5 +8,13 @@ public class BinaryTreeUsingLinkedList {
         this.root =null;
     }
 
-    
+    //Preorder Traversal
+    void PreOrderTraversal(BinaryNode node){
+        if(node==null){
+            return;
+        }
+        System.out.print(node.value+" ");
+        PreOrderTraversal(node.left);
+        PreOrderTraversal(node.right);
+    }
 }
