d1490dfdc173d68674466bc1ee320c17a5d4c6fb
krishna <dhekkrishna1@gmail.com>
2023-08-06 17:36:21 +0530

    Added second approach

--- LeetCodeSolutions/FindTheIndexOfFirstOccurrenceString.java
+++ LeetCodeSolutions/FindTheIndexOfFirstOccurrenceString.java
@@ -1,33 +1,44 @@
+// Leetcode 28.
+// Given two strings needle and haystack, return the index of the first 
+// occurrence of needle in haystack, or -1 if needle is not part of haystack.
 package LeetCodeSolutions;
 
 public class FindTheIndexOfFirstOccurrenceString {
     public static int FirstIndex(String hayStack, String needle){
-        int count =0;
-        int hayStackLen = hayStack.length();
-        int needleLen = needle.length();
+        // int count =0;
+        // int hayStackLen = hayStack.length();
+        // int needleLen = needle.length();
 
-        if(hayStackLen<needleLen)
-            return -1;
+        // if(hayStackLen<needleLen)
+        //     return -1;
 
-        for(int i=0; i<hayStackLen; i++){
-            if(hayStack.charAt(i)==needle.charAt(count)){
-                count++;
+        // for(int i=0; i<hayStackLen; i++){
+        //     if(hayStack.charAt(i)==needle.charAt(count)){
+        //         count++;
+        //     }
+        //     else if(hayStack.charAt(i)!=needle.charAt(count)){
+        //         i = i-count;
+        //         count =0;
+        //     }
+        //     if(count==needleLen){
+        //         return i -=count-1;
+        //     }
+        // }
+        // return -1;
+
+
+            if(hayStack.contains(needle)){
+                return hayStack.indexOf(needle);
+
             }
-            else if(hayStack.charAt(i)!=needle.charAt(count)){
-                i = i-count;
-                count =0;
-            }
-            if(count==needleLen){
-                return i -=count-1;
-            }
-        }
-        return -1;
+             return -1;  
+            
 
     }
 
     public static void main(String[] args) {
-        String hayStack = "leeetcode";
-        String needle = "codeleet";
+        String hayStack = "leetcode";
+        String needle = "code";
        int ans = FirstIndex(hayStack,needle);
         System.out.println(ans);
     }
