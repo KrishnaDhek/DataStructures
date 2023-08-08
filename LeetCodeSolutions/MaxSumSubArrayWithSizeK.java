d49022605466e3f5f3602103ed19f00d5e99ed2c
krishna <dhekkrishna1@gmail.com>
2023-08-09 01:11:06 +0530

    Solution to find maximum subarray sum with window size k

--- LeetCodeSolutions/MaxSumSubArrayWithSizeK.java
+++ LeetCodeSolutions/MaxSumSubArrayWithSizeK.java
@@ -7,35 +7,36 @@ package LeetCodeSolutions;
 import java.util.Arrays;
 
 public class MaxSumSubArrayWithSizeK {
-    public static int MaxSumSubArrayWithSizeK(int[] arr, int k){
+    public static int[] MaxSumSubArrayWithSizeK(int[] arr, int k){
 
         int start =0;
         int maxSum = 0;
         int sum =0;
-        int end =0;
+        
 
+        int[] a = new int[k];
         for(int i=0; i<arr.length; i++){
             sum += arr[i];
 
             if(i>=k-1){
-                end =i;
+               
                 maxSum = Math.max(maxSum, sum);
-
                 sum -=arr[start];
                 start++;
                 
             }
             
         }
-        
-        return maxSum;
+
+        System.out.println(start);
+        return a;
 
     }
 
     public static void main(String[] args) {
         int[] arr = {2,1,5,1,3,2};
         int k=3;
-       int a = MaxSumSubArrayWithSizeK(arr, k);
+       int[] a = MaxSumSubArrayWithSizeK(arr, k);
        
         System.out.println("Maximum sum of subarray with windo size k is :" +a);
        
