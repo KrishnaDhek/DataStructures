abf8a1bc8f92d9e781c050c400bddfac59186f9f
krishna <dhekkrishna1@gmail.com>
2023-08-10 19:07:47 +0530

    Added insert method in Trie

--- javaCoding/Trie/Main.java
+++ javaCoding/Trie/Main.java
@@ -3,6 +3,10 @@ package Trie;
 public class Main {
     public static void main(String[] args) {
          Trie trie = new Trie();
+         trie.insert("API");
+         trie.insert("APPI");
+         trie.insert("APIS");
+
     }
    
     
--- javaCoding/Trie/Trie.java
+++ javaCoding/Trie/Trie.java
@@ -8,4 +8,19 @@ public class Trie {
         root = new TrieNode();
         System.out.println("Trie has been created!");
     }
+
+    public void insert(String word){
+        TrieNode currNode  = root;
+        for(int i =0; i<word.length(); i++){
+            char ch = word.charAt(i); 
+            TrieNode node  = currNode.children.get(ch);
+            if(node==null){
+                node = new TrieNode();
+                currNode.children.put(ch, node);
+            }
+            currNode = node;
+        }
+        currNode.endOfString = true;
+        System.out.println("Successfully inserted "+word+" in Trie");
+    }
 }
