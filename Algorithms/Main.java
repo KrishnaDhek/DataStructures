985d7ba237efe86bd0b5fe4337cda8e2d947851d
krishna <dhekkrishna1@gmail.com>
2023-08-11 16:43:46 +0530

    Refoctor

--- Algorithms/Main.java
+++ Algorithms/Main.java
@@ -3,7 +3,7 @@ package Algorithms;
 public class Main {
     public static void main(String[] args) {
         BubbleSort bSort = new BubbleSort();
-        int[] arr = {10,5,3,6,9,2,1};
+        int[] arr = {10,5,5,3,6,9,2,1};
         bSort.BubbleSort(arr);
         bSort.print(arr);
     }
