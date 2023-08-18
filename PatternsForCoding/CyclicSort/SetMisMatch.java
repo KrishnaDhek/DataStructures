a1449547cb4885f3ab397380637f28443fec2d69
krishna <dhekkrishna1@gmail.com>
2023-08-18 16:17:18 +0530

    Cyclic sort to find the duplicate number and the missing number

---
+++ PatternsForCoding/CyclicSort/SetMisMatch.java
@@ -0,0 +1,38 @@
+package PatternsForCoding.CyclicSort;
+
+public class SetMisMatch {
+    public static int[] SetMisMatch(int[] nums){
+        int i =0;
+        while(i<nums.length){
+            int pos = nums[i]-1;
+            if(nums[i]<nums.length &&nums[i]!=nums[pos]){
+                swap(nums, i, pos);
+            }
+            else{
+                i++;
+            }
+        }
+
+        for(int j =0; j<nums.length; j++){
+            if(nums[j]!=j+1){
+                return new int[]{nums[j],j+1};
+            }
+        }
+        return new int[]{-1,-1};
+
+    }
+    public static void swap(int[] nums, int i, int pos){
+        int temp = nums[i];
+        nums[i] =nums[pos];
+        nums[pos] =temp;
+    }
+
+    public static void main(String[] args) {
+        int[] nums ={1,5,6,6,2,3,4};
+        int[] a = SetMisMatch(nums);
+        for(int i: a)
+
+        System.out.print(i+" ");
+    }
+    
+}
