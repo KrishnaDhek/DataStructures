85a8053d36437ed9ab6dbe787a02245d69872e28
krishna <dhekkrishna1@gmail.com>
2023-08-20 15:55:39 +0530

    Leetcode problem number 287, find the duplicate number using cyclic sort

--- PatternsForCoding/CyclicSort/FindTheDuplicateNumber.java
+++ PatternsForCoding/CyclicSort/FindTheDuplicateNumber.java
@@ -1,5 +1,8 @@
 package PatternsForCoding.CyclicSort;
-
+// Given an array of integers nums containing n + 1 integers where each integer
+//  is in the range [1, n] inclusive.
+// There is only one repeated number in nums, return this repeated number.
+// You must solve the problem without modifying the array nums and uses only constant extra space.
 public class FindTheDuplicateNumber {
     public static int FindTheDuplicateNumber(int[] nums){
         int i =0; 
