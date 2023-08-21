8ee0a8b26a783a0ebef56ff13a8ead0800dc5d5e
krishna <dhekkrishna1@gmail.com>
2023-08-21 15:14:00 +0530

    Added method checkStatus

--- OOPs/ClassFan.java
+++ OOPs/ClassFan.java
@@ -1,5 +1,8 @@
 package OOPs;
 class Fan{
+    // instance variable
+    // initialised everytime we create object, can be accessed from anywhere
+
     private boolean isOn;
     public void turnOn(){
         isOn = true;
@@ -9,13 +12,23 @@ class Fan{
         isOn = false;
         System.out.println("Fan is off");
     }
+
+    public void checkStatus(){
+        if(isOn){
+            System.out.println("Fan is ON");
+        }
+        System.out.println("Fan is OFF");
+    }
 }
 
 public class ClassFan {
     public static void main(String[] args) {
         Fan fan = new Fan();
+        Fan fan2 = new Fan();
         fan.turnOn();
-        fan.turnOff();
+        fan.checkStatus();
+        fan2.checkStatus();
+        fan2.turnOff();
         
     }
     
