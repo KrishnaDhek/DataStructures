852fbee373ce6b9264fc3270f5cd6a38403f2217
krishna <dhekkrishna1@gmail.com>
2023-08-22 19:43:50 +0530

    Zig-Zag traversal of Binary Tree

---
+++ PatternsForCoding/TreeBreadthFirstSearch/ZigZagTraversal.java
@@ -0,0 +1,66 @@
+package PatternsForCoding.TreeBreadthFirstSearch;
+import java.util.*;
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {}
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+public class ZigZagTraversal{
+    public static List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+        List<List<Integer>> result = new ArrayList<>();
+        if (root == null) return result;
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        boolean zigzag = false; // false means left to right, true means right to left
+
+        while (!queue.isEmpty()) {
+            Deque<Integer> levelList = new LinkedList<>();
+            int size = queue.size();
+
+            for (int i = 0; i < size; i++) {
+                TreeNode currentNode = queue.poll();
+                if (!zigzag) {
+                    levelList.addLast(currentNode.val); // Add to the end for left to right
+                } else {
+                    levelList.addFirst(currentNode.val); // Add to the front for right to left
+                }
+
+                if (currentNode.left != null) queue.offer(currentNode.left);
+                if (currentNode.right != null) queue.offer(currentNode.right);
+            }
+
+            result.add(new ArrayList<>(levelList));
+            zigzag = !zigzag; // Switch the direction for the next level
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(3, new TreeNode(9), new TreeNode(20, new TreeNode(15), new TreeNode(7)));
+        
+        List<List<Integer>> result = zigzagLevelOrder(root);
+        for (List<Integer> level : result) {
+            System.out.println(level);
+        }
+    }
+}
+
+
+    
+
