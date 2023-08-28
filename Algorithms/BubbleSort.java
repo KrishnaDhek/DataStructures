fc699001c1e986e012a3f7b029076601d06dca02
krishna <dhekkrishna1@gmail.com>
2023-08-28 15:11:22 +0530

    Bubble sort in increasing order

--- Algorithms/BubbleSort.java
+++ Algorithms/BubbleSort.java
@@ -9,7 +9,7 @@ public class BubbleSort {
 
         for(int i =0; i<len-1; i++){
             for(int j =0; j<len-i-1; j++){
-                if(arr[j]<arr[j+1]){
+                if(arr[j]>arr[j+1]){
                     int temp = arr[j];
                     arr[j] = arr[j+1];
                     arr[j+1] =temp;
