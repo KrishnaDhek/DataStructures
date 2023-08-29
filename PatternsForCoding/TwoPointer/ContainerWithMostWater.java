b1c5dc0e7d717f29131f9add254caa1008d7e0bf
krishna <dhekkrishna1@gmail.com>
2023-08-30 01:32:08 +0530

    Deatiled solution to find the maximum area that can accumulate most of the water

--- PatternsForCoding/TwoPointer/ContainerWithMostWater.java
+++ PatternsForCoding/TwoPointer/ContainerWithMostWater.java
@@ -7,10 +7,13 @@ public class ContainerWithMostWater {
         int maxWater =0;
 
         while(left<right){
+            // to avoid overflow we need to find the minimum length
             int len = Math.min(height[left], height[right]);
+            // to find the max distance between two indices
             int width = right-left;
             maxWater = Math.max(maxWater, len*width);
-
+            // as the width is decreasing, we need to find the max hight
+            // so that we can accumulate maximum water
             if(height[left]<height[right]){
                 left++;
             }
