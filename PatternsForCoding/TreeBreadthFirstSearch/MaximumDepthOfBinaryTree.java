b7d4fa6b102d18ca371a9ba02255a529c53a109e
krishna <dhekkrishna1@gmail.com>
2023-08-30 03:58:34 +0530

    Using Queue to find the maximum depth of the tree

--- PatternsForCoding/TreeBreadthFirstSearch/MaximumDepthOfBinaryTree.java
+++ PatternsForCoding/TreeBreadthFirstSearch/MaximumDepthOfBinaryTree.java
@@ -1,5 +1,8 @@
 package PatternsForCoding.TreeBreadthFirstSearch;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 class BNode{
     int data;
     BNode left;
@@ -15,5 +18,40 @@ class BNode{
     }
 }
 public class MaximumDepthOfBinaryTree {
+    public static int maxDepth(BNode root){
+        if(root==null){
+            return 0;
+        }
+
+        int depth =0;
+        Queue<BNode> q = new LinkedList<>();
+        q.offer(root);
+
+
+        while(!q.isEmpty()){
+            int level = q.size();
+            for(int i =0; i<level; i++){
+                BNode cNode =   q.poll();
+
+                if(cNode.left!=null)
+                q.add(cNode.left);
+
+                if(cNode.right!=null)
+                q.add(cNode.right);
+            }
+            depth++;
+        }
+        return depth;
+    }
+    public static void main(String[] args) {
+        BNode root = new BNode(1);
+        root.left = new BNode(2);
+        root.right = new BNode(3);
+        root.left.right = new BNode(4);
+        root.right.left = new BNode(5);
+        root.right.right = new BNode(6);
+        root.right.left.left = new BNode(7);
+        System.out.println(maxDepth(root));
+    }
     
 }
