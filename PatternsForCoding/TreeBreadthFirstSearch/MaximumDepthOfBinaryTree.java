3fca60173bd6fb6d9ca79fb735bcf27fc479fc78
krishna <dhekkrishna1@gmail.com>
2023-08-30 03:48:35 +0530

    Skeleton to find the max depth

---
+++ PatternsForCoding/TreeBreadthFirstSearch/MaximumDepthOfBinaryTree.java
@@ -0,0 +1,19 @@
+package PatternsForCoding.TreeBreadthFirstSearch;
+
+class BNode{
+    int data;
+    BNode left;
+    BNode right;
+    BNode(){}
+    BNode(int val){
+        this.data = val;
+    }
+    BNode(int val, BNode left, BNode right){
+        this.data = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+public class MaximumDepthOfBinaryTree {
+    
+}
