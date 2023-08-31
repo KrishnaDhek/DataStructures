83422a7a19f86304a49d1323b718d07018287f48
krishna <dhekkrishna1@gmail.com>
2023-08-31 23:12:33 +0530

    Adding constructor to tree

--- PatternsForCoding/TreeDepthFirstSearch/AllPathSum.java
+++ PatternsForCoding/TreeDepthFirstSearch/AllPathSum.java
@@ -1,5 +1,18 @@
 package PatternsForCoding.TreeDepthFirstSearch;
-
+class NodeOfTree{
+    int data;
+    NodeOfTree left;
+    NodeOfTree right;
+    NodeOfTree(){}
+    NodeOfTree(int val){
+        this.data = val;
+    }
+    NodeOfTree(int val, NodeOfTree left, NodeOfTree right){
+        this.data = val;
+        this.left = left;
+        this.right = right;
+    }
+}
 public class AllPathSum {
     
 }
