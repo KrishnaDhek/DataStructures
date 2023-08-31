50f0fed9db6e1f45163d246e68c9e5cb29c33e36
krishna <dhekkrishna1@gmail.com>
2023-08-31 23:34:33 +0530

    Finding path from the root-to-leaf that constitutes to the given sum

--- PatternsForCoding/TreeDepthFirstSearch/AllPathSum.java
+++ PatternsForCoding/TreeDepthFirstSearch/AllPathSum.java
@@ -1,4 +1,9 @@
 package PatternsForCoding.TreeDepthFirstSearch;
+
+import java.util.List;
+
+import java.util.ArrayList;
+
 class NodeOfTree{
     int data;
     NodeOfTree left;
@@ -14,5 +19,49 @@ class NodeOfTree{
     }
 }
 public class AllPathSum {
+    private static List<NodeOfTree> pathList = new ArrayList<>();
+    public static boolean hasPathSum(NodeOfTree root, int target){
+        if(root==null)
+            return false;
+
+            pathList.add(root);
+        target -=root.data;
+
+        if(root.left==null && root.right==null && target==0){
+            PrintThePath();
+            return true;
+            
+        }
+        if(hasPathSum(root.left, target) || hasPathSum(root.right, target)){
+            return true;
+        }
+        
+        pathList.remove(pathList.size()-1);
+        return false;
+    }
+
+    private static void PrintThePath(){
+        int size = pathList.size();
+        for(int i=0; i<size; i++){
+            System.out.print(pathList.get(i).data);
+            if(i<size-1){
+                System.out.print("->");
+            }
+        }
+        System.out.println();
+    }
+    public static void main(String[] args) {
+        NodeOfTree root = new NodeOfTree(5);
+        root.left = new NodeOfTree(4);
+        root.left.left = new NodeOfTree(11);
+        root.left.left.left = new NodeOfTree(7);
+        root.left.left.right = new NodeOfTree(2);
+        root.right = new NodeOfTree(8);
+        root.right.left = new NodeOfTree(13);
+        root.right.right = new NodeOfTree(4);
+        root.right.right.right =new NodeOfTree(1);
+        int target = 26;
+        System.out.println("Does tree has a path that is equal to the given sum from root-to-leaf ? " +hasPathSum(root, target ));
+    }
     
 }
