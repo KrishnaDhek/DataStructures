76c4ae2eef80ef6fbb0f36d716c011ebd69d90f4
krishna <dhekkrishna1@gmail.com>
2023-09-05 12:33:35 +0530

    Check whether a number is palindrome or not

--- LeetCodeSolutions/PalindromeNumber.java
+++ LeetCodeSolutions/PalindromeNumber.java
@@ -1,5 +1,27 @@
 package LeetCodeSolutions;
 
 public class PalindromeNumber {
+    public static boolean palinNum(int x){
+        if(x<0)
+            return false;
+            int n = x;
+            int result , ans =0;
+
+        while(x>0){
+            result = x%10;
+            ans = ans*10+result;
+            x = x/10;
+        }
+
+        if(ans==n){
+            return true;
+        }
+        return false;
+
+    }
+    public static void main(String[] args) {
+        int n = 1456541;
+        System.out.println(palinNum(n));
+    }
     
 }
