f2409c6ac2c59d58c37c5d52209bb487e9c50655
krishna <dhekkrishna1@gmail.com>
2023-09-05 22:41:38 +0530

    Climbing stair

--- LeetCodeSolutions/ClimbingStair.java
+++ LeetCodeSolutions/ClimbingStair.java
@@ -1,5 +1,12 @@
 package LeetCodeSolutions;
 
 public class ClimbingStair {
+    public static int countDistinctWays(int n){
+
+    }
+    public static void main(String[] args) {
+        int n = 9;
+        System.out.println(countDistinctWays(n));
+    }
     
 }
