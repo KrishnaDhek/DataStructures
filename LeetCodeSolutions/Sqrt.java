a762e36bc99da27a16f5b5a9f1c0cc69d26bc8d2
krishna <dhekkrishna1@gmail.com>
2023-09-05 21:52:27 +0530

    Square root of a number

--- LeetCodeSolutions/Sqrt.java
+++ LeetCodeSolutions/Sqrt.java
@@ -1,5 +1,12 @@
 package LeetCodeSolutions;
 
 public class Sqrt {
+    public static int numSqrt(int x){
+
+    }
+    public static void main(String[] args) {
+        int n = 2456889;
+        System.out.println(numSqrt(n));
+    }
     
 }
