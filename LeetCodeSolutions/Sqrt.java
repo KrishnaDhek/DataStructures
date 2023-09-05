fafdeba27fcc217de1482ff0b98dd491b85f4e03
krishna <dhekkrishna1@gmail.com>
2023-09-05 21:55:09 +0530

    Binary search to ind the square root of anumber

--- LeetCodeSolutions/Sqrt.java
+++ LeetCodeSolutions/Sqrt.java
@@ -2,6 +2,24 @@ package LeetCodeSolutions;
 
 public class Sqrt {
     public static int numSqrt(int x){
+        int left =1;
+        int right =x;
+
+        while(left<=right){
+            int mid = left+(right-left);
+
+            if((long)mid*mid ==x){
+                return mid;
+            }
+            if((long)mid*mid<x){
+
+                left =mid+1;
+
+            } else {
+                right = mid-1;
+            }
+        }
+        return right;
 
     }
     public static void main(String[] args) {
