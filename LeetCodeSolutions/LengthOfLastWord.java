2fcdfe05944adbf1c3dbf4c590eced85dfce6489
krishna <dhekkrishna1@gmail.com>
2023-09-05 22:21:35 +0530

    Finding the length of last word of the string

--- LeetCodeSolutions/LengthOfLastWord.java
+++ LeetCodeSolutions/LengthOfLastWord.java
@@ -2,6 +2,14 @@ package LeetCodeSolutions;
 
 public class LengthOfLastWord {
     public static int lengOfLastWord(String s){
+        s =s.trim();
+        int count =0;
+        int j = s.length()-1;
+        while(j>=0 && s.charAt(j)!=' '){
+            count++;
+            j--;
+        }
+        return count;
 
     }
     public static void main(String[] args) {
