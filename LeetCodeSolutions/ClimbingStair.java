639c91592d2bec6c53af035654491906c1bb5ce3
krishna <dhekkrishna1@gmail.com>
2023-09-05 22:46:13 +0530

    Distinct ways to climb a stairs

--- LeetCodeSolutions/ClimbingStair.java
+++ LeetCodeSolutions/ClimbingStair.java
@@ -2,6 +2,16 @@ package LeetCodeSolutions;
 
 public class ClimbingStair {
     public static int countDistinctWays(int n){
+        int a =1, b =2, c =0;
+        int i =3;
+        while(i<=n){
+            c = a+b;
+            a = b;
+            b = c;
+            i++;
+            
+        }
+        return c;
 
     }
     public static void main(String[] args) {
