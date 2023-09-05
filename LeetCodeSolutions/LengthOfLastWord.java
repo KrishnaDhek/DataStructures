36519ece36ba7395fc712047067e2345c6b2d7ea
krishna <dhekkrishna1@gmail.com>
2023-09-05 22:19:57 +0530

    method to find the length of last word

--- LeetCodeSolutions/LengthOfLastWord.java
+++ LeetCodeSolutions/LengthOfLastWord.java
@@ -1,5 +1,12 @@
 package LeetCodeSolutions;
 
 public class LengthOfLastWord {
+    public static int lengOfLastWord(String s){
+
+    }
+    public static void main(String[] args) {
+        String s = " fly to the moon ";
+        System.out.println(lengOfLastWord(s));
+    }
     
 }
