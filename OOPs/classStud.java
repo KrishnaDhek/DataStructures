49f853228da6fc74fe9d9bebffa7d0c0920129d4
krishna <dhekkrishna1@gmail.com>
2023-09-12 20:28:18 +0530

    Working of student class

---
+++ OOPs/classStud.java
@@ -0,0 +1,37 @@
+package OOPs;
+class stud{
+    public int roll;
+    public String name;
+    public String course;
+    public int m1,m2,m3;
+
+    public int total(){
+        return m1+m2+m3;
+    }
+
+    public double avg(){
+        return total()/3;
+    }
+
+    public char grade(){
+        if(avg()>=60){
+            return 'A';
+        }
+        else{
+            return 'B';
+        }
+    }
+}
+public class classStud {
+    public static void main(String[] args) {
+        stud S = new stud();
+        S.roll =12;
+        S.name = "John";
+        S.course ="CS";
+        S.m1 =50;
+        S.m2 =49;
+        S.m3 =45;
+        System.out.println("Total :"+S.total());
+        System.out.println("Average :"+S.avg());
+    }
+}
