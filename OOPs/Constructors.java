a69c0fe703dfa3f9abf079b76f520ae4f288c03a
krishna <dhekkrishna1@gmail.com>
2023-09-13 01:33:56 +0530

    Created a class rectangle

--- OOPs/Constructors.java
+++ OOPs/Constructors.java
@@ -1,5 +1,11 @@
 package OOPs;
-
+class Rectangle{
+    private int length;
+    private int width;
+    private int bredth;
+}
 public class Constructors {
-    
+    public static void main(String[] args) {
+        Rectangle r = new Rectangle();
+    }
 }
