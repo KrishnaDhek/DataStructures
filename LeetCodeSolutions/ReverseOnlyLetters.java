34465de637fc561b7e1dbe2c23967091470a1fa5
krishna <dhekkrishna1@gmail.com>
2023-09-13 18:23:57 +0530

    Reversing letters of the string

--- LeetCodeSolutions/ReverseOnlyLetters.java
+++ LeetCodeSolutions/ReverseOnlyLetters.java
@@ -2,7 +2,27 @@ package LeetCodeSolutions;
 
 public class ReverseOnlyLetters {
     public static String reverseOnlyLetters(String s){
-        
+
+        char[] ch = s.toCharArray();
+        int i=0;
+        int j= s.length()-1;
+
+        while(i<j){
+            while(i<j && !Character.isLetter(ch[i])){
+                i++;
+            }
+            while(i<j && !Character.isLetter(ch[j])){
+                j--;
+            }
+
+            char temp =ch[i];
+            ch[i] = ch[j];
+            ch[j] =temp;
+            i++;
+            j--;
+        }
+        return new String(ch);
+
     }
     public static void main(String[] args) {
         String s = "a-bC-dEF-ghIj";
