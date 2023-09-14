975a9c7fe1f81969f97bd3cab075ad4e0cd78d1c
krishna <dhekkrishna1@gmail.com>
2023-09-15 03:15:27 +0530

    Created a subClass Cylinder

--- OOPs/Inheritance.java
+++ OOPs/Inheritance.java
@@ -6,7 +6,7 @@ class Circle{
     }
 
     public Circle(int r){
-        setRadius(Radius);
+        setRadius(r);
     }
 
     public int getRadius(){
@@ -25,11 +25,40 @@ class Circle{
         return "Radius of circle is :"+Radius;
     }
 } 
+
+class Cylinder extends Circle{
+    private double height;
+
+    public Cylinder(){
+        super();
+
+    }
+    public Cylinder(int r, double h){
+        super(r);
+        setheight(h);
+    }
+    
+    public Cylinder(Circle c){
+        super(c.getRadius());
+
+    }
+
+    public void setheight(double h){
+       this.height = h;
+    }
+    public String toString(){
+        return "Radius of cylinder is :"+getRadius()+"\n"+"Height of cyclinder is :"+height;
+    }
+    
+}
 public class Inheritance {
     public static void main(String[] args) {
         Circle c = new Circle();
-        c.setRadius(2);
+        c.setRadius(9);
         System.out.println(c);
+        Cylinder cy = new Cylinder(c);
+        cy.setheight(2.1);
+        System.out.println(cy);
     }
     
 }
