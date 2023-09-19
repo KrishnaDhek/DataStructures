c76a30db47c291d2c6d39d6597e2ec8a77751553
krishna <dhekkrishna1@gmail.com>
2023-09-19 14:24:35 +0530

    Animal class created

--- OOPs/ParametrisedConstructor.java
+++ OOPs/ParametrisedConstructor.java
@@ -1,5 +1,28 @@
 package OOPs;
 
+class Animal{
+    public Animal(){
+        System.out.println("This is parent class");
+    }
+    public Animal(String type){
+        switch(type.toLowerCase()){
+            case "mammal":
+            System.out.println("Animal belongs to mammal category");
+            break;
+            case "ambhibian":
+            System.out.println("Animal belongs to ambhinian category");
+            break;
+            default:
+            System.out.println("Animal category un-known");
+            break;
+        }
+       
+    }
+
+}
+
+
+
 public class ParametrisedConstructor {
     
 }
