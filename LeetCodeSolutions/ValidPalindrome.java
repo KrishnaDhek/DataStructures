7c2ebf8332236e4dd836016c2580b7fbc074334f
krishna <dhekkrishna1@gmail.com>
2023-09-19 19:28:36 +0530

    LeetCode solution numnber 125. Valid Palindrome

--- LeetCodeSolutions/ValidPalindrome.java
+++ LeetCodeSolutions/ValidPalindrome.java
@@ -2,11 +2,22 @@ package LeetCodeSolutions;
 
 public class ValidPalindrome {
     public static boolean validPaln(String s){
+        s = s.toLowerCase();
+        String str = s.replaceAll("[^a-zA-Z0-9]","");
 
+        int i =0; 
+        int j =str.length()-1;
+
+        while(i<j){
+            if(str.charAt(i++)!=str.charAt(j--))
+            return false;
+
+        }
+        return true;
     }
     public static void main(String[] args) {
         String s = "A man, a plan, a canal: Panama";
-        System.out.println("Is string a vlid palindrome :"+validPaln(s));
+        System.out.println("Is string a valid palindrome :"+validPaln(s));
     }
 
     
