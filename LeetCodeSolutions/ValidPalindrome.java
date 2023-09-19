29bb924def05d11a0d4fe4e9ef567fe6f6d1711c
krishna <dhekkrishna1@gmail.com>
2023-09-19 18:09:18 +0530

    Method that checks for a valid palindrome

--- LeetCodeSolutions/ValidPalindrome.java
+++ LeetCodeSolutions/ValidPalindrome.java
@@ -1,6 +1,13 @@
 package LeetCodeSolutions;
 
 public class ValidPalindrome {
+    public static boolean validPaln(String s){
+
+    }
+    public static void main(String[] args) {
+        String s = "A man, a plan, a canal: Panama";
+        System.out.println("Is string a vlid palindrome :"+validPaln(s));
+    }
 
     
     
