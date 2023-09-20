b4424d6f202b5b3e598a7d4fd12c3231edc2a1dd
krishna <dhekkrishna1@gmail.com>
2023-09-20 13:54:59 +0530

    Method maxProfit, finds the maximum profit to buy and sell stock

--- LeetCodeSolutions/BestTimeToBuyAndSellStock.java
+++ LeetCodeSolutions/BestTimeToBuyAndSellStock.java
@@ -2,7 +2,15 @@ package LeetCodeSolutions;
 
 public class BestTimeToBuyAndSellStock {
     public static int maxProfit(int[] prices){
-        
+        int profit =0;
+        for(int i=1; i<prices.length; i++){
+            if(prices[i]> prices[i-1]){
+                profit +=prices[i]-prices[i-1];
+            }
+
+        }
+        return profit;
+
     }
     public static void main(String[] args) {
         int[] prices = {7,1,5,3,6,4};
