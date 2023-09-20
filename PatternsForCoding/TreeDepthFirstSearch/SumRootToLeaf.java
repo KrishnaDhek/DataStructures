f719330a701223005a62811e06a962ded874d1a4
krishna <dhekkrishna1@gmail.com>
2023-09-20 17:40:33 +0530

    Created TreeNode class

--- PatternsForCoding/TreeDepthFirstSearch/SumRootToLeaf.java
+++ PatternsForCoding/TreeDepthFirstSearch/SumRootToLeaf.java
@@ -1,5 +1,22 @@
 package PatternsForCoding.TreeDepthFirstSearch;
+class TreeNode{
+    int data;
+    TreeNode left;
+    TreeNode right;
 
+    TreeNode(){}
+    TreeNode(int val){
+        data = val;
+    }
+    TreeNode(int data, TreeNode left, TreeNode right){
+        this.data = data;
+        this.left = left;
+        this.right = right;
+    }
+}
 public class SumRootToLeaf {
+    public static void main(String[] args) {
+        
+    }
     
 }
