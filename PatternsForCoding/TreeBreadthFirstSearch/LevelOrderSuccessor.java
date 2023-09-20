2daa607a9ee20d2663b836b256d0df6952c02cc6
krishna <dhekkrishna1@gmail.com>
2023-09-20 18:02:24 +0530

    Method call to levelSuccessor

--- PatternsForCoding/TreeBreadthFirstSearch/LevelOrderSuccessor.java
+++ PatternsForCoding/TreeBreadthFirstSearch/LevelOrderSuccessor.java
@@ -15,5 +15,18 @@ class TreeNode{
     }
 }
 public class LevelOrderSuccessor {
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(1);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(3);
+        root.left.left = new TreeNode(4);
+        root.left.right = new TreeNode(5);
+        root.right.left = new TreeNode(6);
+        root.right.right = new TreeNode(9);
+        root.left.left.left= new TreeNode(7);
+        root.left.left.right =new TreeNode(8);
+        root.right.left.right = new TreeNode(10);
+        System.out.println(levelSuccessor(root));
+}
     
 }
