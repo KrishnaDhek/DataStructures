26a5de907494f305a841d3182b9ab3100f88536a
krishna <dhekkrishna1@gmail.com>
2023-09-20 18:08:28 +0530

    Level Order Successor of a given node

--- PatternsForCoding/TreeBreadthFirstSearch/LevelOrderSuccessor.java
+++ PatternsForCoding/TreeBreadthFirstSearch/LevelOrderSuccessor.java
@@ -1,4 +1,8 @@
 package PatternsForCoding.TreeBreadthFirstSearch;
+
+import java.util.Queue;
+import java.util.LinkedList;
+
 class TreeNode{
     int val;
     TreeNode left;
@@ -15,6 +19,26 @@ class TreeNode{
     }
 }
 public class LevelOrderSuccessor {
+    public static TreeNode levelSuccessor(TreeNode root, int key){
+        if(root ==null)
+            return null;
+
+        Queue<TreeNode> q = new  LinkedList<>();
+        q.offer(root);
+        while(!q.isEmpty()){
+            TreeNode cNnode = q.poll();
+            if(cNnode.left!=null)
+                q.offer(cNnode.left);
+            if(cNnode.right!=null)
+                q.offer(cNnode.right);
+
+            if(cNnode.val == key)
+                    break;
+
+        }
+        return q.peek();
+
+    }
     public static void main(String[] args) {
         TreeNode root = new TreeNode(1);
         root.left = new TreeNode(2);
@@ -26,7 +50,14 @@ public class LevelOrderSuccessor {
         root.left.left.left= new TreeNode(7);
         root.left.left.right =new TreeNode(8);
         root.right.left.right = new TreeNode(10);
-        System.out.println(levelSuccessor(root));
+        TreeNode result  = levelSuccessor(root, 3);
+
+        if(result!=null){
+            System.out.println(result.val+" ");
+        }
+        else{
+            System.out.println("No successor found");
+        }
 }
     
 }
