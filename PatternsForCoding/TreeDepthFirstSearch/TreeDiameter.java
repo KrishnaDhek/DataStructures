26a9bbe730f3f1926a2e6418d49e2cd582b6946f
krishna <dhekkrishna1@gmail.com>
2023-09-21 00:35:23 +0530

    Defined the tree node class

--- PatternsForCoding/TreeDepthFirstSearch/TreeDiameter.java
+++ PatternsForCoding/TreeDepthFirstSearch/TreeDiameter.java
@@ -1,5 +1,21 @@
 package PatternsForCoding.TreeDepthFirstSearch;
 
+import apple.laf.JRSUIUtils.Tree;
+
+class TreeNode{
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(){}
+    TreeNode(int val){
+        this.val =val;
+    }
+    TreeNode(int val, TreeNode left, TreeNode right){
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
 public class TreeDiameter {
     
 }
