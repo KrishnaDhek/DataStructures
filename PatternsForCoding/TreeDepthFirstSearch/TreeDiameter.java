81f4c047085c93e7065981f918dd85d96e1d81e2
krishna <dhekkrishna1@gmail.com>
2023-09-21 00:41:32 +0530

    diameter of a binary tree is the length of the longest path between any two nodes in the tree

--- PatternsForCoding/TreeDepthFirstSearch/TreeDiameter.java
+++ PatternsForCoding/TreeDepthFirstSearch/TreeDiameter.java
@@ -1,6 +1,5 @@
 package PatternsForCoding.TreeDepthFirstSearch;
 
-import apple.laf.JRSUIUtils.Tree;
 
 class TreeNode{
     int val;
@@ -17,8 +16,22 @@ class TreeNode{
     }
 }
 public class TreeDiameter {
+    private static int dia = 0;
     public static int depthOfTree(TreeNode root){
-        
+        depth(root);
+        return dia;
+    }
+
+    private static int depth(TreeNode root){
+        if(root==null)
+            return 0;
+
+        int leftDepth = depth(root.left);
+        int rightDepth = depth(root.right);
+
+        dia =Math.max(dia, leftDepth+rightDepth);
+
+        return Math.max(leftDepth, rightDepth)+1;
     }
     public static void main(String[] args) {
         TreeNode root = new TreeNode(1);
@@ -26,6 +39,7 @@ public class TreeDiameter {
         root.left.left = new TreeNode(3);
         root.left.right = new TreeNode(4);
         root.right = new TreeNode(5);
+        root.right.left = new TreeNode(6);
         System.out.println(depthOfTree(root));
     }
     
