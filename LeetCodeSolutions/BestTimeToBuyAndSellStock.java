e175b76462acbf451841ea11c77f29737f31556e
krishna <dhekkrishna1@gmail.com>
2023-09-20 13:48:56 +0530

    Main method and function call

--- LeetCodeSolutions/BestTimeToBuyAndSellStock.java
+++ LeetCodeSolutions/BestTimeToBuyAndSellStock.java
@@ -1,5 +1,9 @@
 package LeetCodeSolutions;
 
 public class BestTimeToBuyAndSellStock {
+    public static void main(String[] args) {
+        int[] prices = {7,1,5,3,6,4};
+        System.out.println("The maximum profir is :"+maxProfit(prices));
+    }
     
 }
