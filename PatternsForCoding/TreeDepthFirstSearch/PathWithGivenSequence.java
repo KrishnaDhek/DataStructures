fec2a3cd2c9206f48efd36bce391c9d252c3049e
krishna <dhekkrishna1@gmail.com>
2023-09-21 00:56:51 +0530

    determine if a binary tree has a root-to-leaf path that matches a given sequence of integers.

--- PatternsForCoding/TreeDepthFirstSearch/PathWithGivenSequence.java
+++ PatternsForCoding/TreeDepthFirstSearch/PathWithGivenSequence.java
@@ -1,15 +1,15 @@
 package PatternsForCoding.TreeDepthFirstSearch;
 class TreeNode{
-    int val;
+    int data;
     TreeNode left;
     TreeNode right;
     TreeNode(){}
     TreeNode(int val){
-        this.val = val;
+        this.data = val;
 
     }
     TreeNode(int val, TreeNode left, TreeNode right){
-        this.val = val;
+        this.data = val;
         this.left = left;
         this.right =right;
 
@@ -17,13 +17,13 @@ class TreeNode{
 }
 
 public class PathWithGivenSequence {
-    public boolean hasPath(TreeNode root, int[] sequence) {
+    public static boolean hasPath(TreeNode root, int[] sequence) {
         if (root == null) return sequence.length == 0;
         return checkPath(root, sequence, 0);
     }
 
-    private boolean checkPath(TreeNode currentNode, int[] sequence, int index) {
-        if (currentNode == null || index >= sequence.length || currentNode.val != sequence[index])
+    private static boolean checkPath(TreeNode currentNode, int[] sequence, int index) {
+        if (currentNode == null || index >= sequence.length || currentNode.data != sequence[index])
             return false;
 
         // If the current node is a leaf and matches the end of the sequence, return true
@@ -33,5 +33,19 @@ public class PathWithGivenSequence {
         // Otherwise, check the left and right subtrees
         return checkPath(currentNode.left, sequence, index + 1) || checkPath(currentNode.right, sequence, index + 1);
     }
-    
+    public static void main(String[] args) {
+        
+        TreeNode root = new TreeNode(1);
+        root.left = new TreeNode(0);
+        root.right = new TreeNode(1);
+        root.left.left = new TreeNode(1);
+        root.right.left = new TreeNode(6);
+        root.right.right = new TreeNode(8);
+        root.right.right.left = new TreeNode(9);
+        root.right.right.right = new TreeNode(0);
+        
+        System.out.println(hasPath(root, new int[] {1, 1, 6}));  // true
+        System.out.println(hasPath(root, new int[] {1, 0, 7}));  // false
+        System.out.println(hasPath(root, new int[] {1, 1, 6, 8}));  // false
+    }
 }
