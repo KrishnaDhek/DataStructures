a78d68398b59ee7f799b722656f60d3d403f1c95
krishna <dhekkrishna1@gmail.com>
2023-09-20 13:49:49 +0530

    Method that finds the maximum profit is defined

--- LeetCodeSolutions/BestTimeToBuyAndSellStock.java
+++ LeetCodeSolutions/BestTimeToBuyAndSellStock.java
@@ -1,6 +1,9 @@
 package LeetCodeSolutions;
 
 public class BestTimeToBuyAndSellStock {
+    public static int maxProfit(int[] prices){
+        
+    }
     public static void main(String[] args) {
         int[] prices = {7,1,5,3,6,4};
         System.out.println("The maximum profir is :"+maxProfit(prices));
