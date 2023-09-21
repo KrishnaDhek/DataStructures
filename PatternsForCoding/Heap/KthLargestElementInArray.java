3f0a505817f445fc4c26728dbcf473afb0f78a6f
krishna <dhekkrishna1@gmail.com>
2023-09-21 16:47:10 +0530

    Kth smallest element in array

--- PatternsForCoding/Heap/KthLargestElementInArray.java
+++ PatternsForCoding/Heap/KthLargestElementInArray.java
@@ -1,8 +1,19 @@
 package PatternsForCoding.Heap;
 
+import java.util.PriorityQueue;
+
 public class KthLargestElementInArray {
     public static int kthLargestEle(int[] nums, int k){
-        
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+
+        for(int i: nums){
+            pq.offer(i);
+
+            if(pq.size()>k){
+                pq.remove();
+            }
+        }
+        return pq.peek();
     }
     public static void main(String[] args) {
         int[] nums= {2,5,7,8,9,90,98,89,134,6};
