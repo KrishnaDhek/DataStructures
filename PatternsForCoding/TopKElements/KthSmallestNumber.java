9cdcdc561f9c18beec1497424acbc0786ec8a01e
krishna <dhekkrishna1@gmail.com>
2023-09-21 18:20:15 +0530

    Method declaration

--- PatternsForCoding/TopKElements/KthSmallestNumber.java
+++ PatternsForCoding/TopKElements/KthSmallestNumber.java
@@ -1,6 +1,9 @@
 package PatternsForCoding.TopKElements;
 
 public class KthSmallestNumber {
+    public static int kthSmallest(int[] nums, int k){
+        
+    }
     public static void main(String[] args) {
         int[] nums = {9,7,5,3,8,6,2,1};
         System.out.println(kthSmallest(nums, 4));
