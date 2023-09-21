61f71ad211a1026fba337be9b47e712c9e4a8840
krishna <dhekkrishna1@gmail.com>
2023-09-21 18:25:11 +0530

    Finding the smallest element using max-Heap

--- PatternsForCoding/TopKElements/KthSmallestNumber.java
+++ PatternsForCoding/TopKElements/KthSmallestNumber.java
@@ -1,8 +1,21 @@
 package PatternsForCoding.TopKElements;
 
+import java.util.PriorityQueue;
+
 public class KthSmallestNumber {
     public static int kthSmallest(int[] nums, int k){
-        
+        PriorityQueue<Integer> p= new PriorityQueue<>((a,b)-> b-a);
+
+        for(int i: nums){
+            p.offer(i);
+
+            if(p.size()>k){
+                p.remove();
+            }
+        }
+        return p.peek();
+
+
     }
     public static void main(String[] args) {
         int[] nums = {9,7,5,3,8,6,2,1};
