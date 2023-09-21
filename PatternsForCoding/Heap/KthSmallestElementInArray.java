944a264b5fba65083f2ed1419403a34ef7e6eca6
krishna <dhekkrishna1@gmail.com>
2023-09-21 16:54:07 +0530

    Main method declaration

--- PatternsForCoding/Heap/KthSmallestElementInArray.java
+++ PatternsForCoding/Heap/KthSmallestElementInArray.java
@@ -1,5 +1,9 @@
 package PatternsForCoding.Heap;
 
 public class KthSmallestElementInArray {
+    public static void main(String[] args) {
+        int[] nums ={2,5,7,8,9,90,98,89,134,6};
+        System.out.println(kthSmallest(nums, 6));
+    }
     
 }
