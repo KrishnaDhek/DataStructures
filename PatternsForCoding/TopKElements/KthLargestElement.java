ca61896cf0779bf0fe9cfa17d89a82996a222d27
krishna <dhekkrishna1@gmail.com>
2023-09-21 18:52:51 +0530

    Largest kth element using max-heap

--- PatternsForCoding/TopKElements/KthLargestElement.java
+++ PatternsForCoding/TopKElements/KthLargestElement.java
@@ -1,8 +1,24 @@
 package PatternsForCoding.TopKElements;
 
+import java.util.PriorityQueue;
+
 public class KthLargestElement {
+    public static int kthLargest(int[] nums, int k){
+        PriorityQueue<Integer> p = new PriorityQueue<>((a,b)-> b-a);
+
+        for(int i :nums){
+            p.offer(i);
+        }
+        int ans=0;
+        for(int i=0; i<k; i++){
+            ans =p.poll();
+        }
+
+        return ans;
+    }
     public static void main(String[] args) {
         int[] nums = {9,2,5,8,1,7,3,6,10,12};
+        System.out.println(kthLargest(nums,4));
 
 
     }
