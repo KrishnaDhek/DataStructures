a7efd9449733e65eb3a38806cfc4d59e24bb60d0
krishna <dhekkrishna1@gmail.com>
2023-09-21 17:12:12 +0530

    finind the kth smallest element in array using max-heap

--- PatternsForCoding/Heap/KthSmallestElementInArray.java
+++ PatternsForCoding/Heap/KthSmallestElementInArray.java
@@ -1,6 +1,20 @@
 package PatternsForCoding.Heap;
 
+import java.util.PriorityQueue;
+
 public class KthSmallestElementInArray {
+    public static int kthSmallest(int[] nums, int k){
+        PriorityQueue<Integer>p = new PriorityQueue<>((a,b)-> b-a);
+
+        for(int i :nums){
+            p.offer(i);
+
+            if(p.size()>k){
+                p.remove();
+            }
+        }
+        return p.peek();
+    }
     public static void main(String[] args) {
         int[] nums ={2,5,7,8,9,90,98,89,134,6};
         System.out.println(kthSmallest(nums, 6));
