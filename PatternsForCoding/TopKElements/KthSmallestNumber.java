a3d32d5006baaf1996aef2f54bdd0d98ee8bf1a0
krishna <dhekkrishna1@gmail.com>
2023-09-21 18:19:40 +0530

    Main method declaration and method calling

---
+++ PatternsForCoding/TopKElements/KthSmallestNumber.java
@@ -0,0 +1,9 @@
+package PatternsForCoding.TopKElements;
+
+public class KthSmallestNumber {
+    public static void main(String[] args) {
+        int[] nums = {9,7,5,3,8,6,2,1};
+        System.out.println(kthSmallest(nums, 4));
+    }
+    
+}
