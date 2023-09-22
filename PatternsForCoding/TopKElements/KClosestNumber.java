18380ef0115590658a5baf03087b9b914f0cb2b3
krishna <dhekkrishna1@gmail.com>
2023-09-22 17:43:56 +0530

    Using binary search as the array is sorted

--- PatternsForCoding/TopKElements/KClosestNumber.java
+++ PatternsForCoding/TopKElements/KClosestNumber.java
@@ -5,7 +5,21 @@ import java.util.List;
 public class KClosestNumber {
 
     public static List<Integer> kClosest(int[] nums, int k, int x){
-        
+
+        int low =0; 
+        int height = nums.length-k;
+
+        while(low<height){
+            int mid = low+(height-low)/2;
+
+            if(x- nums[mid]> nums[mid+k]-k){
+                low = mid+1;
+            }
+            else{
+                height = mid;
+            }
+        }
+
     }
     public static void main(String[] args) {
         int[] arr = {1,2,3,4,5};
