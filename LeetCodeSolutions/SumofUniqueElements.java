f79f8eb7387ad4b084772bbad44c2291e0877770
krishna <dhekkrishna1@gmail.com>
2023-09-22 23:28:32 +0530

    Finding the sum of unique elements

--- LeetCodeSolutions/SumofUniqueElements.java
+++ LeetCodeSolutions/SumofUniqueElements.java
@@ -3,14 +3,12 @@ package LeetCodeSolutions;
 import java.util.List;
 
 public class SumofUniqueElements {
-    public static  List<Integer> sumOfUnique(int[] n){
-        
+    public static int sumOfUnique(int[] n){
+
     }
     public static void main(String[] args) {
         int[] n ={2,3,2,4,5,5,1,7};
-
-        List<Integer> list = sumOfUnique(n);
-        System.out.println(list);
+        System.out.println(sumOfUnique(n));
     }
     
 }
