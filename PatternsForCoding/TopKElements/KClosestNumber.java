c4f19657241fc199452189b823e079525344601a
krishna <dhekkrishna1@gmail.com>
2023-09-22 17:38:42 +0530

    Main method declared

--- PatternsForCoding/TopKElements/KClosestNumber.java
+++ PatternsForCoding/TopKElements/KClosestNumber.java
@@ -1,5 +1,12 @@
 package PatternsForCoding.TopKElements;
 
 public class KClosestNumber {
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,4,5};
+        int k =4;
+        int x =3;
+
+        List<Integer> list = kClosest(arr, k, x);
+    }
     
 }
