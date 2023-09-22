ab14f17104785c34aa80624b0bcc151b72fa22ea
krishna <dhekkrishna1@gmail.com>
2023-09-22 23:26:25 +0530

    A sum method declared that returns a list

--- LeetCodeSolutions/SumofUniqueElements.java
+++ LeetCodeSolutions/SumofUniqueElements.java
@@ -1,8 +1,12 @@
 package LeetCodeSolutions;
 
+import java.util.List;
+
 public class SumofUniqueElements {
     public static void main(String[] args) {
-        
+        int[] n ={2,3,2,4,5,5,1,7};
+
+        List<Integer> list = sumOfUnique(n);
     }
     
 }
