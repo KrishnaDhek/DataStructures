b868f4d8f94100dfcc7ca2d59f6d6e05b2ceb262
krishna <dhekkrishna1@gmail.com>
2023-09-22 18:27:20 +0530

    Finding all the is a number which is divisible by every digit it contain, except zero

--- LeetCodeSolutions/SelfDividingNumber.java
+++ LeetCodeSolutions/SelfDividingNumber.java
@@ -1,11 +1,36 @@
 package LeetCodeSolutions;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class SelfDividingNumber {
 
     public static List<Integer> selfDividingNumber(int left, int right){
-        
+
+        List<Integer> list = new ArrayList<>();
+
+        for(int i= left; i<=right; i++){
+            if(divisible(i)){
+                list.add(i);
+            }
+        }
+        return list;
+    }
+
+    private static boolean divisible(int i){
+        int temp = i;
+
+        while(temp>0){
+            int ans = temp%10;
+
+            if(ans==0 || i%ans !=0){
+                return false;
+            }
+
+            temp = temp/10;
+        }
+
+        return true;
     }
     public static void main(String[] args) {
         int left =1;
