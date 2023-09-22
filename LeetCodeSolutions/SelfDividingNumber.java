b1e2f05a719e62192eb99780d2038f541c8e8745
krishna <dhekkrishna1@gmail.com>
2023-09-22 18:14:47 +0530

    Main method declatred and method call

--- LeetCodeSolutions/SelfDividingNumber.java
+++ LeetCodeSolutions/SelfDividingNumber.java
@@ -1,5 +1,13 @@
 package LeetCodeSolutions;
 
+import java.util.List;
+
 public class SelfDividingNumber {
+    public static void main(String[] args) {
+        int left =1;
+        int right =22;
+         List<Integer> list = selfDivingNumber(left,right);
+         System.out.println(list);
+    }
     
 }
