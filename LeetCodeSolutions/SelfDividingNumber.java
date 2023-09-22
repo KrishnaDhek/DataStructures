304345b2d86f353a9711c3e0f167a0dafec2a67f
krishna <dhekkrishna1@gmail.com>
2023-09-22 18:16:28 +0530

    Method initialization, that will return a list

--- LeetCodeSolutions/SelfDividingNumber.java
+++ LeetCodeSolutions/SelfDividingNumber.java
@@ -3,10 +3,14 @@ package LeetCodeSolutions;
 import java.util.List;
 
 public class SelfDividingNumber {
+
+    public static List<Integer> selfDividingNumber(int left, int right){
+        
+    }
     public static void main(String[] args) {
         int left =1;
         int right =22;
-         List<Integer> list = selfDivingNumber(left,right);
+         List<Integer> list = selfDividingNumber(left,right);
          System.out.println(list);
     }
     
