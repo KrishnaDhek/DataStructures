cc82a1e7376e04a6262ada423152aaa8a4d0bbc2
krishna <dhekkrishna1@gmail.com>
2023-09-22 23:27:43 +0530

    MEthod sumOfUnique elementes declared

--- LeetCodeSolutions/SumofUniqueElements.java
+++ LeetCodeSolutions/SumofUniqueElements.java
@@ -3,6 +3,9 @@ package LeetCodeSolutions;
 import java.util.List;
 
 public class SumofUniqueElements {
+    public static  List<Integer> sumOfUnique(int[] n){
+        
+    }
     public static void main(String[] args) {
         int[] n ={2,3,2,4,5,5,1,7};
 
