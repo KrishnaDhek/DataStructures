d466014704e22a97c9488f9147dfebf799217b7c
krishna <dhekkrishna1@gmail.com>
2023-09-24 03:07:11 +0530

    Method call to subsetSum

--- PatternsForCoding/Dynamicprogramming/EqualSubsetSumPartition.java
+++ PatternsForCoding/Dynamicprogramming/EqualSubsetSumPartition.java
@@ -6,6 +6,11 @@ public class EqualSubsetSumPartition {
         for(int i: nums){
             sum+=i;
         }
+
+        if(sum%2!=0)
+            return false;
+
+        return subsetSum(nums, sum/2);
     }
     public static void main(String[] args) {
         int[] nums ={1,5,11,5};
