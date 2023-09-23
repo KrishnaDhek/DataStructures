2d3939f1f57329a98c3816cfe3520656e93187f8
krishna <dhekkrishna1@gmail.com>
2023-09-24 02:40:38 +0530

    Finding the smallest kth element from a sorted matrix, using binary search

--- PatternsForCoding/K_WayMerge/KthSmallestElementinASortedMatrix.java
+++ PatternsForCoding/K_WayMerge/KthSmallestElementinASortedMatrix.java
@@ -1,10 +1,44 @@
 package PatternsForCoding.K_WayMerge;
 
 public class KthSmallestElementinASortedMatrix {
+    public static int kthSmallestInMatrix(int[][] matrix, int k){
+        int low =matrix[0][0];
+        int hight = matrix[matrix.length-1][matrix.length-1];
+
+        while(low<hight){
+            int mid = low+(hight-low)/2;
+
+            int countVal = countNum(matrix, mid);
+
+            if(countVal<k){
+                low = mid+1;
+            }
+            else{
+                hight = mid;
+            }
+        }
+        return low;
+    }
+
+    private static int countNum(int[][] matrix, int mid){
+        int count = 0;
+        int j = matrix.length-1;
+
+        for(int i=0; i<matrix.length; i++){
+            while(j>=0 && matrix[i][j]>mid){
+                j--;
+            }
+
+            count +=(j+1);
+        }
+
+        return count;
+
+    }
     public static void main(String[] args) {
         int[][] matrix = {{1,  5,  9},{10, 11, 13},{12, 13, 15}};
         int k = 6;
-        System.out.println(kthSmallestInMatrix(matrix));
+        System.out.println(kthSmallestInMatrix(matrix,k));
     }
     
 }
