d0bbb041d0fb15895d155cc44a8fa813027c266b
krishna <dhekkrishna1@gmail.com>
2023-09-24 02:49:18 +0530

    Declared value, weight and total capacity of knapsack

--- PatternsForCoding/Dynamicprogramming/Knapsack.java
+++ PatternsForCoding/Dynamicprogramming/Knapsack.java
@@ -1,5 +1,10 @@
 package PatternsForCoding.Dynamicprogramming;
 
 public class Knapsack {
-    
+    public static void main(String[] args) {
+        int[] val = { 60, 100, 120 };
+        int[] weight ={ 10, 20, 30 };
+        int total = 50;
+        System.out.println(knapSack(val, weight, total));
+    }
 }
