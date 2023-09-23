7036014a645743c546cc765be7d99ba93d25b16e
krishna <dhekkrishna1@gmail.com>
2023-09-24 02:04:44 +0530

    A matrix declared, with the targeted largest element to be find

--- PatternsForCoding/K_WayMerge/KthSmallestElementinASortedMatrix.java
+++ PatternsForCoding/K_WayMerge/KthSmallestElementinASortedMatrix.java
@@ -1,5 +1,10 @@
 package PatternsForCoding.K_WayMerge;
 
 public class KthSmallestElementinASortedMatrix {
+    public static void main(String[] args) {
+        int[][] matrix = {{1,  5,  9},{10, 11, 13},{12, 13, 15}};
+        int k = 6;
+        System.out.println(kthSmallestInMatrix(matrix));
+    }
     
 }
