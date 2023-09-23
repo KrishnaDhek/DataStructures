0f519c9607f7441a25f6a6e115ae5537b8b71a79
krishna <dhekkrishna1@gmail.com>
2023-09-24 01:36:50 +0530

    Created two sorted list, method call to merge these list

--- PatternsForCoding/K_WayMerge/MergeSortedList.java
+++ PatternsForCoding/K_WayMerge/MergeSortedList.java
@@ -8,5 +8,19 @@ class Node{
     }
 }
 public class MergeSortedList {
+    public static void main(String[] args) {
+        Node head = new Node(1);
+        head.next = new Node(2);
+        head.next.next = new Node(3);
+        head.next.next = new Node(5);
+
+        Node head2 = new Node(4);
+        head2.next = new Node(6);
+        head2.next.next = new Node(7);
+        head2.next.next.next = new Node(9);
+
+        Node temp = mergeList(head,head2);
+
+    }
     
 }
