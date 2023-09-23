7a0773583d76c57b92d29ff0e6f527fe9b306d19
krishna <dhekkrishna1@gmail.com>
2023-09-23 13:35:04 +0530

    Used hash map to store the frequency of each character, and a maxHeap to create maxHeap based on the highest frequency

--- PatternsForCoding/TopKElements/FrequencySort.java
+++ PatternsForCoding/TopKElements/FrequencySort.java
@@ -1,7 +1,24 @@
 package PatternsForCoding.TopKElements;
 
+import java.util.HashMap;
+import java.util.PriorityQueue;
+
 public class FrequencySort {
     public static String frequencySort(String s){
+        HashMap<Character,Integer> map = new HashMap<>();
+
+        for(char c : s.toCharArray()){
+            if(map.containsKey(c)){
+                map.put(c, map.get(c)+1);
+            }
+            else{
+                map.put(c, 1);
+            }
+        }
+
+        PriorityQueue<Character> p = new PriorityQueue<>((a,b)-> map.get(b) - map.get(a));
+
+        p.addAll(map.keySet());
         
     }
     public static void main(String[] args) {
