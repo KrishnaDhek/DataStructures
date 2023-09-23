2cbfb97bb4356eeaec93fbcf2298a535b5f2e1ac
krishna <dhekkrishna1@gmail.com>
2023-09-24 03:07:58 +0530

    Method subsetSum declared

--- PatternsForCoding/Dynamicprogramming/EqualSubsetSumPartition.java
+++ PatternsForCoding/Dynamicprogramming/EqualSubsetSumPartition.java
@@ -12,6 +12,10 @@ public class EqualSubsetSumPartition {
 
         return subsetSum(nums, sum/2);
     }
+
+    private static boolean subsetSum(int[] a, int s){
+        
+    }
     public static void main(String[] args) {
         int[] nums ={1,5,11,5};
         System.out.println(equalSum(nums));
