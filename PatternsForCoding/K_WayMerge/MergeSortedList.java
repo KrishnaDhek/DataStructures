967b9880cfc639883ae87ae7bae2c469f94d111a
krishna <dhekkrishna1@gmail.com>
2023-09-24 01:31:20 +0530

    Created node of the tree

---
+++ PatternsForCoding/K_WayMerge/MergeSortedList.java
@@ -0,0 +1,12 @@
+package PatternsForCoding.K_WayMerge;
+class Node{
+    int data;
+    Node next;
+
+    Node(int data){
+        this.data = data;
+    }
+}
+public class MergeSortedList {
+    
+}
