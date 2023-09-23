9dc10e89362540121f951b6f90930adfbe617c83
krishna <dhekkrishna1@gmail.com>
2023-09-24 01:43:04 +0530

    Merged the sorted list

--- PatternsForCoding/K_WayMerge/MergeSortedList.java
+++ PatternsForCoding/K_WayMerge/MergeSortedList.java
@@ -32,15 +32,21 @@ public class MergeSortedList {
     public static void main(String[] args) {
         Node head = new Node(1);
         head.next = new Node(2);
-        head.next.next = new Node(3);
-        head.next.next = new Node(5);
+        head.next.next = new Node(6);
+        head.next.next.next = new Node(10);
 
         Node head2 = new Node(4);
-        head2.next = new Node(6);
-        head2.next.next = new Node(7);
-        head2.next.next.next = new Node(9);
+        head2.next = new Node(8);
+        head2.next.next = new Node(17);
+        head2.next.next.next = new Node(29);
 
         Node temp = mergeList(head,head2);
+
+        while(temp!=null){
+            System.out.print(temp.data +"->");
+            temp = temp.next;
+        }
+        System.out.print("null");
 
     }
     
