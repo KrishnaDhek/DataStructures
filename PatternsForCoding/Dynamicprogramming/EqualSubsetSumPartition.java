37ed71f6bbdd824e8d14557dd9255a0ed509908e
krishna <dhekkrishna1@gmail.com>
2023-09-24 03:05:23 +0530

    Method equalSum declared

--- PatternsForCoding/Dynamicprogramming/EqualSubsetSumPartition.java
+++ PatternsForCoding/Dynamicprogramming/EqualSubsetSumPartition.java
@@ -1,6 +1,9 @@
 package PatternsForCoding.Dynamicprogramming;
 
 public class EqualSubsetSumPartition {
+    public static boolean equalSum(int[] nums){
+        
+    }
     public static void main(String[] args) {
         int[] nums ={1,5,11,5};
         System.out.println(equalSum(nums));
