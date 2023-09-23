3e73e75d6fbca1be57a8edf9bfbe5c55e4204ec1
krishna <dhekkrishna1@gmail.com>
2023-09-24 03:04:35 +0530

    main method that decalres an array

--- PatternsForCoding/Dynamicprogramming/EqualSubsetSumPartition.java
+++ PatternsForCoding/Dynamicprogramming/EqualSubsetSumPartition.java
@@ -1,5 +1,9 @@
 package PatternsForCoding.Dynamicprogramming;
 
 public class EqualSubsetSumPartition {
+    public static void main(String[] args) {
+        int[] nums ={1,5,11,5};
+        System.out.println(equalSum(nums));
+    }
     
 }
