86291c5b8ed3bcc968b148fdabe92e1c2844c30b
krishna <dhekkrishna1@gmail.com>
2023-09-24 03:15:18 +0530

    SunsetSum using dynamic programming

---
+++ PatternsForCoding/Dynamicprogramming/SubsetSum.java
@@ -0,0 +1,34 @@
+package PatternsForCoding.Dynamicprogramming;
+
+public class SubsetSum {
+
+    public static boolean isSubsetSum(int[] nums, int target) {
+        int n = nums.length;
+
+        boolean[][] dp = new boolean[n + 1][target + 1];
+
+        for (int i = 0; i <= n; i++) {
+            dp[i][0] = true;
+        }
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= target; j++) {
+                if (nums[i - 1] > j) {
+                    dp[i][j] = dp[i - 1][j];
+                } else {
+                    dp[i][j] = dp[i - 1][j] || dp[i - 1][j - nums[i - 1]];
+                }
+            }
+        }
+
+        return dp[n][target];
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {3, 34, 4, 12, 5, 2};
+        int target = 9;
+
+        System.out.println(isSubsetSum(nums, target));  
+    }
+}
+
