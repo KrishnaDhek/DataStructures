87f33fcc3e85f9c8d2e99484c261d17fba93da70
krishna <dhekkrishna1@gmail.com>
2023-09-26 23:23:34 +0530

    Using minHeap to find the largest kth element

--- PatternsForCoding/TopKElements/KthLargestElement.java
+++ PatternsForCoding/TopKElements/KthLargestElement.java
@@ -4,17 +4,16 @@ import java.util.PriorityQueue;
 
 public class KthLargestElement {
     public static int kthLargest(int[] nums, int k){
-        PriorityQueue<Integer> p = new PriorityQueue<>((a,b)-> b-a);
+        PriorityQueue<Integer> p = new PriorityQueue<>();
 
         for(int i :nums){
             p.offer(i);
-        }
-        int ans=0;
-        for(int i=0; i<k; i++){
-            ans =p.poll();
+        
+        if(p.size()>k)
+            {p.poll();}
         }
 
-        return ans;
+        return p.peek();
     }
     public static void main(String[] args) {
         int[] nums = {9,2,5,8,1,7,3,6,10,12};
