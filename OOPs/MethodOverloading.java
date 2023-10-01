b98827e670797d7f22bd99707fea860d6bbd64b9
krishna <dhekkrishna1@gmail.com>
2023-10-01 15:10:19 +0530

    Method overloading in java, same mwthod with different parameter

--- OOPs/MethodOverloading.java
+++ OOPs/MethodOverloading.java
@@ -2,12 +2,23 @@ package OOPs;
 
 public class MethodOverloading {
 
+  int Sum(int i, int j){
+    return i+j;
+  }
+
+  double Sum(double i, double j){
+    return i+j;
+  }
+
+  int Sum(int i , int j, int k){
+    return i+j+k;
+  }
     public static void main(String[] args) {
         MethodOverloading methOver = new MethodOverloading();
 
-        System.out.println("Sum of Integers is :" methOver.Sum(20,-9));
-         System.out.println("Sum of Integers is :" methOver.Sum(20.98,45.8));
-          System.out.println("Sum of Integers is :" methOver.Sum(20,40,-9));
+        System.out.println("Sum of two Integers is :"+ methOver.Sum(20,-9));
+         System.out.println("Sum of two Double is :" +methOver.Sum(20.98,45.8));
+          System.out.println("Sum of three Integers is :" +methOver.Sum(20,40,-9));
     }
     
 }
