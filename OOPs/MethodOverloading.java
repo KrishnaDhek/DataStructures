cea3f20b300b632ae2268f60bb362a092e9a4480
krishna <dhekkrishna1@gmail.com>
2023-10-01 15:04:46 +0530

    Calling various method from main

--- OOPs/MethodOverloading.java
+++ OOPs/MethodOverloading.java
@@ -1,5 +1,11 @@
 package OOPs;
 
 public class MethodOverloading {
+
+    public static void main(String[] args) {
+        MethodOverloading methOver = new MethodOverloading();
+
+        System.out.println("Sum of Integers is :" methOver.Sum(20,-9));
+    }
     
 }
