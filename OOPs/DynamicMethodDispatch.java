6c6caa5101d1b7a520ce75d2403822d011da5216
krishna <dhekkrishna1@gmail.com>
2023-10-03 17:12:22 +0530

    A super class with 3 methods are created

--- OOPs/DynamicMethodDispatch.java
+++ OOPs/DynamicMethodDispatch.java
@@ -1,5 +1,17 @@
 package OOPs;
+class SuperClass{
+    public static void meth1(){
+        System.out.println("Method 1 of superClass");
+    }
 
+    public static void meth2(){
+        System.out.println("Method 2 of superClass");
+    }
+
+    public static void meth3(){
+        System.out.println("Method 3 of supeClass");
+    }
+}
 public class DynamicMethodDispatch {
     
 }
