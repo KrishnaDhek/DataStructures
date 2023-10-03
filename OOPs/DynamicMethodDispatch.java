0a2e2f0b6ae57dea2de8e1e8094d2d69f42ed916
krishna <dhekkrishna1@gmail.com>
2023-10-03 17:14:16 +0530

    A subClass is created with 4 methods, which extens(inherites) the superClass

--- OOPs/DynamicMethodDispatch.java
+++ OOPs/DynamicMethodDispatch.java
@@ -12,6 +12,23 @@ class SuperClass{
         System.out.println("Method 3 of supeClass");
     }
 }
+class SubClass extends SuperClass{
+    public static void meth1(){
+        System.out.println("Method 1 of subClass");
+    }
+
+    public static void meth2(){
+        System.out.println("Method 2 of subClass");
+    }
+
+    public static void meth3(){
+        System.out.println("Method 3 of subClass");
+    }
+
+    public static void meth4(){
+        System.out.println("Method 3 of subClass");
+    }
+}
 public class DynamicMethodDispatch {
     
 }
