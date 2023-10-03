fae57bec004372eb3faf2a0e36df918538a98fb9
krishna <dhekkrishna1@gmail.com>
2023-10-03 17:57:47 +0530

    Dynamic Polymorphism is achieved through method overriding, occurs when a subclass provides a specific implementation of a method which is already defined in the superClass

--- OOPs/RunTimePolymorphism.java
+++ OOPs/RunTimePolymorphism.java
@@ -1,5 +1,30 @@
 package OOPs;
 
+class Animals{
+
+    public void Sound()
+    {
+        System.out.println("Animal make a sound ");
+    }
+}
+
+class Cat extends Animals{
+    public void Sound(){
+        System.out.println("Cat meows ");
+    }
+}
+
+class Elephant extends Animals{
+    public void Sound(){
+        System.out.println("Elephant  snorts");
+    }
+}
 public class RunTimePolymorphism {
-    
+    public static void main(String[] args) {
+        Animals a;
+        a = new Cat();
+        a.Sound();
+        a = new Elephant();
+        a.Sound();
+    }
 }
