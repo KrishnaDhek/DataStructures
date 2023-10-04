fbe7f7bd87548003de83ae93f6d7d8e7a1a1b5e0
krishna <dhekkrishna1@gmail.com>
2023-10-04 22:59:56 +0530

    Created a concrete class Rectangle that implements area method

--- OOPs/Abstraction.java
+++ OOPs/Abstraction.java
@@ -3,6 +3,23 @@ package OOPs;
 abstract class Shape{
     abstract double area();
 }
+
+class Rectangle extends Shape{
+    private double length;
+    private double bredth;
+
+    Rectangle(double length, double bredth){
+        this.length = length;
+        this.bredth = bredth;
+    }
+
+    @Override
+
+    double area(){
+         return length*bredth;
+    }
+
+}
 public class Abstraction {
     
 }
