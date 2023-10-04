3b9767f499608c612de6a2789fa5aa7b0f455e0f
krishna <dhekkrishna1@gmail.com>
2023-10-04 23:06:20 +0530

    Abstraction using abstract class

--- OOPs/Abstraction.java
+++ OOPs/Abstraction.java
@@ -1,14 +1,14 @@
 package OOPs;
 
-abstract class Shape{
+abstract class SHP{
     abstract double area();
 }
 
-class Rectangle extends Shape{
+class Rect extends SHP{
     private double length;
     private double bredth;
 
-    Rectangle(double length, double bredth){
+    Rect(double length, double bredth){
         this.length = length;
         this.bredth = bredth;
     }
@@ -22,7 +22,7 @@ class Rectangle extends Shape{
 }
 public class Abstraction {
     public static void main(String[] args) {
-        Shape s = new Rectangle(12.9, 34.5);
-        System.out.println("Area of Rectangle is");
+        SHP s = new Rect(12.9, 34.5);
+        System.out.println("Area of Rectangle is :" +s.area());
     }
 }
