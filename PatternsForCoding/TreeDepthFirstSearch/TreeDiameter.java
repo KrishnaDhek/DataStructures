d892c5639b71f3391092be9b11d1b52fa0e8de76
krishna <dhekkrishna1@gmail.com>
2023-10-05 23:33:04 +0530

    Treediameter of a tree is the number of nodes on the longest path between two leaves in the tree.

--- PatternsForCoding/TreeDepthFirstSearch/TreeDiameter.java
+++ PatternsForCoding/TreeDepthFirstSearch/TreeDiameter.java
@@ -40,6 +40,7 @@ public class TreeDiameter {
         root.left.right = new TreeNode(4);
         root.right = new TreeNode(5);
         root.right.left = new TreeNode(6);
+        root.right.right = new TreeNode(8);
         System.out.println(depthOfTree(root));
     }
     
