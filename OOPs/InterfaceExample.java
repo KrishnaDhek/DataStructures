900d83e141cb5fed6df89044a41899b123facbd0
krishna <dhekkrishna1@gmail.com>
2023-10-27 00:07:27 +0530

    Creating a class circle that implements the interface shape

--- OOPs/InterfaceExample.java
+++ OOPs/InterfaceExample.java
@@ -21,6 +21,23 @@ class Rectangle implements Shape{
         return 2*(len+width);
     }
 }
+
+class Circle implements Shape{
+    private double radius;
+    private final double pi = 3.14159;
+    Circle(double radius){
+        this.radius = radius;
+    }
+
+    @Override
+    public double area(){
+        return pi*radius*radius;
+    }
+    @Override
+    public double parimeter(){
+        return 2*pi*radius;
+    }
+}
 public class InterfaceExample {
     
 }
