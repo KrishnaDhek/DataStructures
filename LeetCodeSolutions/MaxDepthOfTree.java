df14953cfa7e663a0ef5268ce5dc4e85131e1a3f
krishna <dhekkrishna1@gmail.com>
2023-10-26 15:18:01 +0530

    Finding the maximum depth of a binary tree

---
+++ LeetCodeSolutions/MaxDepthOfTree.java
@@ -0,0 +1,22 @@
+package LeetCodeSolutions;
+class TreeChild{
+    int data;
+    TreeChild left;
+    TreeChild right;
+
+    TreeChild(){}
+
+    TreeChild(int data){
+        this.data = data;
+    }
+
+    TreeChild(int data, TreeChild left, TreeChild right){
+        this.data = data;
+        this.left = left;
+        this.right = right;
+
+    }
+}
+public class MaxDepthOfTree {
+    
+}
