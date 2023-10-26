487bd31124b6f11c35101695be2e213543ed6183
krishna <dhekkrishna1@gmail.com>
2023-10-26 15:22:18 +0530

    Defined the main method, and method call to find the max depth of the tree

--- LeetCodeSolutions/MaxDepthOfTree.java
+++ LeetCodeSolutions/MaxDepthOfTree.java
@@ -18,5 +18,17 @@ class TreeChild{
     }
 }
 public class MaxDepthOfTree {
+    public static void main(String[] args) {
+        TreeChild root =  new TreeChild(3);
+        root.left = new TreeChild(9);
+        root.right = new TreeChild(20);
+        root.left.left = new TreeChild(11);
+        root.left.right = new TreeChild(34);
+        root.left.right.right =new TreeChild(44);
+        root.right.left = new TreeChild(5);
+        root.right.left.left = new TreeChild(56);
+        root.right.left.left.left = new TreeChild(78);
+        int depth = MaxDepthOfTree(root);
+    }
     
 }
