33d7a1979ff44c06a117e720965e12589955d942
krishna <dhekkrishna1@gmail.com>
2023-10-26 23:52:59 +0530

    If we are not providing implementation for every method of interface, than we should define that class as abstract, then next level child class is reponsible to provide implementation

--- OOPs/Interface.java
+++ OOPs/Interface.java
@@ -5,6 +5,31 @@ interface Interf{
     void meth1();
     void meth2();
 }
+
+abstract class Test implements Interf{
+    public void meth1(){
+        System.out.println("This is method 1");
+    }
+
+}
+
+class Sample extends Test{
+    public void meth2(){
+        System.out.println("This is method 2");
+    }
+
+    void meth3(){
+        System.out.println("This is method 3");
+    }
+}
 public class Interface {
+    public static void main(String[] args) {
+       Sample s = new Sample();
+       s.meth1();
+       s.meth2();
+       s.meth3();
+
+    }
+    
     
 }
