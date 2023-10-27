c459a720298a0525e55217444bc2db4d2ebd7f39
krishna <dhekkrishna1@gmail.com>
2023-10-27 12:20:38 +0530

    A class Bike implements the interface Vehical

--- OOPs/InterfaceVehical.java
+++ OOPs/InterfaceVehical.java
@@ -6,8 +6,46 @@ package OOPs;
 interface Vehical {
     void start();
     void stop();
-    int maxSpeed();  
+    int getmaxSpeed();  
+}
+
+/**
+ * Bike implements Vehiccal
+ 
+ */
+ class Bike implements Vehical {
+
+    private int maxSpeed;
+
+    Bike(int maxSpeed){
+        this.maxSpeed = maxSpeed;
+    }
+
+    @Override
+    public void start(){
+        System.out.println("Bike is starting");
+    }
+
+    @Override
+    public void stop(){
+        System.out.println("Bike has stoped");
+    }
+
+    @Override
+    public int getmaxSpeed(){
+        return maxSpeed;
+    }
+
+
+    
 }
 public class InterfaceVehical {
+    public static void main(String[] args) {
+        Vehical bike = new Bike(45);
+        bike.start();
+        bike.stop();
+        System.out.println(bike.getmaxSpeed());
+
+    }
     
 }
