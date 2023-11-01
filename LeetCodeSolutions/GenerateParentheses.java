38046fe065ef5fe1aa5d9d2e65002d9dfdd9e4c3
krishna <dhekkrishna1@gmail.com>
2023-11-01 23:54:32 +0530

    Call to helper method

--- LeetCodeSolutions/GenerateParentheses.java
+++ LeetCodeSolutions/GenerateParentheses.java
@@ -1,5 +1,13 @@
 package LeetCodeSolutions;
 
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class GenerateParentheses {
-    
+    public List<String> generateParenthesis(int n) {
+        List<String> result = new ArrayList<>();
+        generateParenthesisHelper(result, "", n, n);
+        return result;
+    }
 }
