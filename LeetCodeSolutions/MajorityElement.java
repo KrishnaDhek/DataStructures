e5b8827ae93fa07d6fdf870debd5d2ad72059054
krishna <dhekkrishna1@gmail.com>
2023-11-06 16:14:23 +0530

    A majority element is one which apreas length/2 times, Using a hashmap we can find the frequency of each element, then via itreating over keys we can find it

--- LeetCodeSolutions/MajorityElement.java
+++ LeetCodeSolutions/MajorityElement.java
@@ -1,6 +1,28 @@
 package LeetCodeSolutions;
 
+import java.util.HashMap;
+
 public class MajorityElement {
+    public static int majorElement(int[] nums){
+
+        HashMap<Integer,Integer> map =new HashMap<>();
+
+        for(int i: nums){
+            if(map.containsKey(i)){
+                map.put(i, map.get(i)+1);
+            }
+            else{
+                map.put(i, 1);
+            }
+        }
+
+        for(int i: map.keySet()){
+            if (map.get(i)> nums.length/2) {
+                return i;
+            }
+        }
+        return -1;
+    }
     public static void main(String[] args) {
         int[] nums= {4,5,4,5,5,4,5,4,5};
 
