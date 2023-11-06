437470023ad522676c996a2aaf81ecdd0a22ae68
krishna <dhekkrishna1@gmail.com>
2023-11-06 16:08:07 +0530

    Method call to find the majority element

--- LeetCodeSolutions/MajorityElement.java
+++ LeetCodeSolutions/MajorityElement.java
@@ -1,5 +1,9 @@
 package LeetCodeSolutions;
 
 public class MajorityElement {
-    
+    public static void main(String[] args) {
+        int[] nums= {4,5,4,5,5,4,5,4,5};
+
+        System.out.println("The element which appreared more than n/2 times is :" +majorElement(nums));
+    }
 }
