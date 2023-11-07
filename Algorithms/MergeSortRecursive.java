38d2f460aa5d5eef2057608e73b6b78aa9421c59
krishna <dhekkrishna1@gmail.com>
2023-11-07 10:57:52 +0530

    Defined a merge sort method that will recursively  divide the array

--- Algorithms/MergeSortRecursive.java
+++ Algorithms/MergeSortRecursive.java
@@ -1,5 +1,12 @@
 package Algorithms;
 
 public class MergeSortRecursive {
+    public static void main(String[] args) {
+        int[] a ={23,0,98,76,4,0,56,7,82,2,54,3,4,169,1,6,9,2};
+        int low = 0;
+        int high = a.length-1;
+// call to merge sort function
+        mergeSort(a, low, high);
+    }
     
 }
