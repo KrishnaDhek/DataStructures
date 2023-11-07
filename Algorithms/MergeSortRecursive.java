334941726f7a067bf1b47dc1a78a6cbe8dd1f18b
krishna <dhekkrishna1@gmail.com>
2023-11-07 11:22:51 +0530

    Implementation of merge sort using recursion

--- Algorithms/MergeSortRecursive.java
+++ Algorithms/MergeSortRecursive.java
@@ -3,6 +3,10 @@ package Algorithms;
 import java.util.ArrayList;
 import java.util.List;
 
+// MERGE SORT IS "Stable" but "Not Inplace"
+// TC- O(NlogN)
+// Sc- O(N)
+
 public class MergeSortRecursive {
     public static void merge(int[] a, int low, int mid, int high){
         // defined a temporary list
@@ -46,6 +50,8 @@ public class MergeSortRecursive {
     public static void mergeSort(int[] a, int low, int high){
         // Base case: If low is greater than or equal to high, the array is already sorted.
         if(low>=high){
+            return;
+        }
             int mid = (low+high)/2;
 
             // recursively call merge sort for the left half
@@ -56,7 +62,7 @@ public class MergeSortRecursive {
 
             // Merge the two sorted halves together.
             merge(a,low,mid,high);
-        }
+        
     }
     public static void main(String[] args) {
         int[] a ={23,0,98,76,4,0,56,7,82,2,54,3,4,169,1,6,9,2};
@@ -64,6 +70,16 @@ public class MergeSortRecursive {
         int high = a.length-1;
 // call to merge sort function
         mergeSort(a, low, high);
+        System.out.println("******Merge Sort******");
+
+        System.out.print("[");
+        for(int i =low; i<=high; i++){
+            System.out.print(a[i]);
+            if(i<a.length-1){
+                System.out.print(",");
+            }
+        }
+        System.out.print("]");
     }
     
 }
