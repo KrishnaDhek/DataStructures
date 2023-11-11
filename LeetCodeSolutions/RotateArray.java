3aebb3bf12b8eee1b510cbab072393a2ebd2bab0
krishna <dhekkrishna1@gmail.com>
2023-11-11 20:51:59 +0530

    Rotate array by k steps

--- LeetCodeSolutions/RotateArray.java
+++ LeetCodeSolutions/RotateArray.java
@@ -26,7 +26,7 @@ public class RotateArray {
 
     }
     public static void main(String[] args) {
-        int[] arr ={1,2,3,4,5,6,7,8,9};
+        int[] arr ={1,2,3,4,5,6,7,8,9,95,125,469,6,81};
         int k =3;
         RotateArray(arr, k);
 
