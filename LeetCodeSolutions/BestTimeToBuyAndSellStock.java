09a98c9380b10a1dd40ec9c62744256fb978a656
krishna <dhekkrishna1@gmail.com>
2023-11-12 21:17:28 +0530

    Using a one pass algorithm to find the maximum profit, for selling and buying stock

--- LeetCodeSolutions/BestTimeToBuyAndSellStock.java
+++ LeetCodeSolutions/BestTimeToBuyAndSellStock.java
@@ -2,15 +2,17 @@ package LeetCodeSolutions;
 
 public class BestTimeToBuyAndSellStock {
     public static int maxProfit(int[] prices){
-        int profit =0;
-        for(int i=1; i<prices.length; i++){
-            if(prices[i]> prices[i-1]){
-                profit +=prices[i]-prices[i-1];
-            }
+       int buy = Integer.MAX_VALUE;
+       int sell =0;
 
+       for(int price : prices){
+        if(price<buy){
+            buy = price;
         }
-        return profit;
-
+        else{
+            sell = Math.max(sell, price-buy);
+        }
+       } return sell;
     }
     public static void main(String[] args) {
         int[] prices = {7,1,5,3,6,4};
